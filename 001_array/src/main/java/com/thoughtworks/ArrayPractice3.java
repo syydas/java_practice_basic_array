a6372c57b4c9867278636ee0d79d88bd04dbbaca
Asuu <suqinzheng_0624@163.com>
2020-02-05 11:27:28 +0800

    [Asu] feat: remove 0 in the array

--- 001_array/src/main/java/com/thoughtworks/ArrayPractice3.java
+++ 001_array/src/main/java/com/thoughtworks/ArrayPractice3.java
@@ -6,7 +6,20 @@ public class ArrayPractice3 {
      * 完成本方法实现功能: 将一个整型数组array中值0的元素去掉，并把剩下的元素组成新的数组
      */
     public static int[] filterZero(int[] array) {
-
-        return null;
+        int newLen = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (0 != array[i]) {
+                newLen++;
+            }
+        }
+        int[] newArray = new int[newLen];
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (0 != array[i]) {
+                newArray[index] = array[i];
+                index++;
+            }
+        }
+        return newArray;
     }
 }
